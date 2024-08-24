2b79738220952e75ec06cdf65fb9207cc2ef15c7
AliAsgharGill <stylinalivlogs@gmail.com>
2024-08-24 16:26:28 +0500

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { IBM_Plex_Serif } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+const ibmPlexSerif = IBM_Plex_Serif({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-ibm-plex-serif",
+});
+
+export const metadata: Metadata = {
+  title: "Fiora",
+  description: "Financial Assistant for Everyone (FAFE) by Fiora",
+  icons: {
+    icon: "/icons/logo.svg",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.variable} ${ibmPlexSerif.variable}`}>
+        {children}
+      </body>
+    </html>
+  );
+}
